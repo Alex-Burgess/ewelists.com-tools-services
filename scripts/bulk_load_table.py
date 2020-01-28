b8335797317057253cc60a3a4926468e5af3fc40
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2020-01-28 10:59:40 +0000

    Added scripts directory with bulk load script.

---
+++ scripts/bulk_load_table.py
@@ -0,0 +1,55 @@
+import json
+import sys
+import boto3
+
+# Test setup
+# usage: python bulk_load_table.py lists-test ../Lists/data/lists-test.json
+# usage: python bulk_load_table.py products-test products-test.json
+# usage: python bulk_load_table.py notfound-test notfound-test.json
+
+# Staging setup
+# usage: python bulk_load_table.py lists-staging ../Lists/data/lists-staging.json
+# usage: python bulk_load_table.py products-staging products-test.json
+# usage: python bulk_load_table.py notfound-staging notfound-test.json
+
+table_name = sys.argv[1]
+file_name = sys.argv[2]
+
+
+dynamodb = boto3.resource('dynamodb')
+
+try:
+    table = dynamodb.Table(table_name)
+except Exception as e:
+    print("Could not connect to table. Error:" + e)
+
+items = []
+
+try:
+    with open(file_name) as f:
+        d = json.load(f)
+        for p in d:
+            items.append(d[p])
+
+    with table.batch_writer() as batch:
+        for item in items:
+            print("Adding item [{}] to table [{}]".format(item['productId'], table_name))
+            batch.put_item(Item=item)
+
+except Exception as e:
+    print("Unexpected Error:" + e)
+
+
+# Old
+# try:
+#     with open(file_name, 'r') as f:
+#         for row in f:
+#             items.append(json.loads(row))
+#
+#     with table.batch_writer() as batch:
+#         for item in items:
+#             print("Adding item [{}] to table [{}]".format(item['productId'], table_name))
+#             # batch.put_item(Item=item)
+#
+# except Exception as e:
+#     print("Unexpected Error:" + e)
