ae6dd5747bbd5c1f6c8319251691ef3b1f9f52b6
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2020-01-29 12:20:36 +0000

    Updates to the comments of tool.

--- scripts/bulk_load_table.py
+++ scripts/bulk_load_table.py
@@ -2,18 +2,13 @@ import json
 import sys
 import boto3
 
-# Test setup
-# usage: python bulk_load_table.py lists-test ../Lists/data/lists-test.json
-# usage: python bulk_load_table.py products-test products-test.json
-# usage: python bulk_load_table.py notfound-test notfound-test.json
-
-# Staging setup
-# usage: python bulk_load_table.py lists-staging ../Lists/data/lists-staging.json
-# usage: python bulk_load_table.py products-staging products-test.json
-# usage: python bulk_load_table.py notfound-staging notfound-test.json
-
 table_name = sys.argv[1]
 file_name = sys.argv[2]
+
+
+# usage: python bulk_load_table.py products-test ../../ewelists.com-web/src/views/ArticlePages/Products/BabyEssentials.json
+# usage: python bulk_load_table.py products-staging ../../ewelists.com-web/src/views/ArticlePages/Products/BabyEssentials.json
+# usage: python bulk_load_table.py products-prod ../../ewelists.com-web/src/views/ArticlePages/Products/BabyEssentials.json
 
 
 dynamodb = boto3.resource('dynamodb')
@@ -41,6 +36,15 @@ except Exception as e:
 
 
 # Old
+# Test setup
+# usage: python bulk_load_table.py lists-test ../Lists/data/lists-test.json
+# usage: python bulk_load_table.py products-test products-test.json
+# usage: python bulk_load_table.py notfound-test notfound-test.json
+
+# Staging setup
+# usage: python bulk_load_table.py lists-staging ../Lists/data/lists-staging.json
+# usage: python bulk_load_table.py products-staging products-test.json
+# usage: python bulk_load_table.py notfound-staging notfound-test.json
 # try:
 #     with open(file_name, 'r') as f:
 #         for row in f:
