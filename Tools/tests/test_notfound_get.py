5b01a14c3a775d3fbe07c5f88b53abb50f027b26
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2020-09-03 21:42:24 +0100

    Implemented logic to query for user and list details in notfound_get function.

--- Tools/tests/test_notfound_get.py
+++ Tools/tests/test_notfound_get.py
@@ -5,11 +5,13 @@ from tools import notfound_get, logger
 log = logger.setup_test_logger()
 
 NOTFOUND_TABLE = 'notfound-unittest'
+LISTS_TABLE = 'lists-unittest'
 
 
 class TestHandler:
     def test_product_missing(self, api_notfound_get_event, monkeypatch, empty_notfound_mock):
         monkeypatch.setitem(os.environ, 'NOTFOUND_TABLE_NAME', NOTFOUND_TABLE)
+        monkeypatch.setitem(os.environ, 'LISTS_TABLE_NAME', LISTS_TABLE)
 
         api_notfound_get_event['pathParameters']['id'] = "12345678-notf-miss-1234-abcdefghijkl"
 
@@ -20,8 +22,9 @@ class TestHandler:
         body = json.loads(response['body'])
         assert body['error'] == 'No product exists with id: 12345678-notf-miss-1234-abcdefghijkl', "response did not contain the correct error message."
 
-    def test_product_exists(self, api_notfound_get_event, monkeypatch, notfound_mock):
+    def test_product_exists(self, api_notfound_get_event, monkeypatch, dynamodb_mock):
         monkeypatch.setitem(os.environ, 'NOTFOUND_TABLE_NAME', NOTFOUND_TABLE)
+        monkeypatch.setitem(os.environ, 'LISTS_TABLE_NAME', LISTS_TABLE)
 
         response = notfound_get.handler(api_notfound_get_event, None)
         assert response['statusCode'] == 200
@@ -33,5 +36,8 @@ class TestHandler:
             "createdBy": "12345678-user-0001-1234-abcdefghijkl",
             "brand": "JL",
             "details": "Safari Mobile",
-            "productUrl": "https://www.johnlewis.com/john-lewis-partners-safari-mobile/p3439165"
+            "productUrl": "https://www.johnlewis.com/john-lewis-partners-safari-mobile/p3439165",
+            "creatorsName": "Test User1",
+            "listTitle": "Child User1 1st Birthday",
+            "listId": "12345678-list-0001-1234-abcdefghijkl"
         }, "Product item was not as expected"
--- Tools/tools/notfound_get.py
+++ Tools/tools/notfound_get.py
@@ -12,18 +12,96 @@ dynamodb = boto3.client('dynamodb')
 
 def handler(event, context):
     try:
-        table_name = common.get_env_variable(os.environ, 'NOTFOUND_TABLE_NAME')
+        notfound_table_name = common.get_env_variable(os.environ, 'NOTFOUND_TABLE_NAME')
+        lists_table_name = common.get_env_variable(os.environ, 'LISTS_TABLE_NAME')
         id = common.get_path_id(event)
 
-        product = get_item(table_name, id)
+        data = get_item(notfound_table_name, id)
 
-        response = common.create_response(200, json.dumps(product))
+        data['creatorsName'] = get_user(lists_table_name, data['createdBy'])
+        data['listId'] = get_list_id(lists_table_name, id)
+        if data['listId']:
+            data['listTitle'] = get_list_title(lists_table_name, data['listId'], data['createdBy'])
+        else:
+            data['listTitle'] = 'Unknown'
+
+        response = common.create_response(200, json.dumps(data))
     except Exception as e:
         log.error("Exception: {}".format(e))
         response = common.create_response(500, json.dumps({'error': str(e)}))
         return response
 
     return response
+
+
+def get_user(table_name, id):
+    key = {
+        'PK': {'S': 'USER#' + id},
+        'SK': {'S': 'USER#' + id}
+    }
+
+    try:
+        response = dynamodb.get_item(
+            TableName=table_name,
+            Key=key
+        )
+    except ClientError as e:
+        log.error("Exception: {}.".format(e))
+        raise Exception("Unexpected problem getting user from lists table.")
+
+    log.info("Get user response: {}".format(response))
+
+    if 'Item' not in response:
+        return None
+
+    return response['Item']['name']['S']
+
+
+def get_list_title(table_name, list_id, user_id):
+    key = {
+        'PK': {'S': 'LIST#' + list_id},
+        'SK': {'S': 'USER#' + user_id}
+    }
+
+    log.info("Querying for key in lists table: {}".format(key))
+
+    try:
+        response = dynamodb.get_item(
+            TableName=table_name,
+            Key=key
+        )
+    except ClientError as e:
+        log.error("Exception: {}.".format(e))
+        raise Exception("Unexpected problem getting list from lists table.")
+
+    log.info("Get list response: {}".format(response))
+
+    if 'Item' not in response:
+        return None
+
+    return response['Item']['title']['S']
+
+
+def get_list_id(table_name, id):
+    log.info("Querying for SK in lists table: PRODUCT#{}".format(id))
+
+    try:
+        response = dynamodb.query(
+            TableName=table_name,
+            IndexName='SK-index',
+            KeyConditionExpression="SK = :SK",
+            ExpressionAttributeValues={":SK":  {'S': "PRODUCT#{}".format(id)}}
+        )
+    except ClientError as e:
+        log.error("Exception: {}.".format(e))
+        raise Exception("Unexpected problem querying for list in lists table.")
+
+    log.info("List Query response: {}".format(response))
+
+    if len(response['Items']) == 0:
+        return None
+
+    return response['Items'][0]['PK']['S'].split("#")[1]
 
 
 def get_item(table_name, id):
