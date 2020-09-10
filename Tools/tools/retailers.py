aa75054dd64fc4138075be5401c479a2b4b84858
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2020-09-10 09:36:33 +0100

    Handled www2 case in retailers module.  Updated postman collections.

--- Tools/tools/retailers.py
+++ Tools/tools/retailers.py
@@ -10,6 +10,8 @@ def get(url):
     domain = urlparse(url).netloc
     if domain.startswith('www.'):
         domain = domain[4:]
+    elif domain.startswith('www2.'):
+        domain = domain[5:]
 
     if domain in map:
         domain = map[domain]
